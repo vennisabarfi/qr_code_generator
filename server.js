6c7636897a11b4960c6d86190ba5ca00f2276ab8
veyhn <109932151+veyhn@users.noreply.github.com>
2024-05-08 11:56:33 -0400

    Solved rendering issues. Qr code generated properly. Issue was with setheader and buffering

--- server.js
+++ server.js
@@ -2,33 +2,36 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 var PNG = require('png-js');
+// const http = require('http');
 
 
 router.get('/qrcode/:text', async function(req,res,next){
     const req_text = req.params.text; // receive text from user
     const text = encodeURIComponent(req_text);
-    const size = "200x200"; // will develop functionality later 
-    const qr_url = 'http://api.qrserver.com/v1/create-qr-code/?data=[URL-encoded-text]&size=[pixels]x[pixels]';
+    const size1 = parseInt('200'); // will develop functionality later 
+    const size2 = parseInt('200');
+    const qr_url = `http://api.qrserver.com/v1/create-qr-code/?data=${text}&size=${size1}x${size2}`;
 
     try{
-        const response = await axios.get(qr_url);
+        const response = await axios.get(qr_url,{
+            // res.setHeader('Content-type', 'image/png')
+            headers:{
+                'Accept': 'image/png'
+            },
+            responseType: 'arraybuffer'
+            });
+            res.setHeader('Content-Type', 'image/png')
             res.send(response.data);
             console.log(response);
         
     }catch(error){
         console.log(`Error: ${error}`);
-        response.json({message: error});
+        res.status(500).json({message: `Error message: ${error}`});
     }
         
 
 
     })
-
-router.get('/qrcode:text', function(req,res,next){
-    req_text
-    next();
-})
-
 
 
 
